08df2de7614bcf922982cb17b0adfa17c387e3c7
Lion User <Vignesh@vigneshs-macbook-pro.local>
2016-06-23 12:56:28 -0500

    Initial Commit

---
+++ reto-calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  reto-calculator
+//
+//  Created by Vignesh Kumar Rajasekaran on 6/23/16.
+//  Copyright © 2016 Vignesh Kumar Rajasekaran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ reto-calculatorTests/reto_calculatorTests.swift
@@ -0,0 +1,36 @@
+//
+//  reto_calculatorTests.swift
+//  reto-calculatorTests
+//
+//  Created by Vignesh Kumar Rajasekaran on 6/23/16.
+//  Copyright © 2016 Vignesh Kumar Rajasekaran. All rights reserved.
+//
+
+import XCTest
+@testable import reto_calculator
+
+class reto_calculatorTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
